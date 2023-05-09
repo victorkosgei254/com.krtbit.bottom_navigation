0bc68258453168d41519940691c34d8d000bfe1e
Victor Kosgei <victorkosgei254@gmail.com>
2023-05-09 12:23:57 +0300

    Bottom navigation example

---
+++ README.md
@@ -0,0 +1,12 @@
+## Bottom Navigation App
+
+This is a demo application on bottom navigation
+
+Screen one
+![Screen one](screen_shot1.png)
+
+screen two 
+![Screen one](screen_shot2.png)
+
+screen three
+![Screen one](screen_shot3.png)
---
+++ app/src/main/java/com/kritbit/bottomnavigation/MainActivity.java
@@ -0,0 +1,46 @@
+package com.kritbit.bottomnavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements  BottomNavigationView.OnNavigationItemSelectedListener{
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,new Shop()).commit();
+                return true;
+            case R.id.cart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,new Cart()).commit();
+                return true;
+            case R.id.shipping:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,new Shipping()).commit();
+                return true;
+
+        }
+        return false;
+    }
+}
